a57c44d4ca5a486a0734fba1fc6be70f7b03b602
mperdikeas <mperdikeas@gmail.com>
2016-06-21 11:55:05 +0300

    exporting DefaultCell as well

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ if (!global._babelPolyfill)
 
 import {MyTableContainer, HighLevelColumnConfiguration}    from './fixed-data-table-wrapper-1-table-smart.js'          ;
 import {TextualColumnSearchHeader, DateColumnSearchHeader} from './fixed-data-table-wrapper-1-column-search-headers.js';
+import {DefaultCell} from './fixed-data-table-wrapper-1-cell-types.js';
 
 exports.MyTableContainer             = MyTableContainer;
 exports.HighLevelColumnConfiguration = HighLevelColumnConfiguration;
 exports.TextualColumnSearchHeader    = TextualColumnSearchHeader;
 exports.DateColumnSearchHeader       = DateColumnSearchHeader;
+exports.DefaultCell                  = DefaultCell;
 
 
